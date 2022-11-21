049c21144e2b64c8fa7b054170684a5c4cf18656
jayeshtekade <100849062+jayeshtekade@users.noreply.github.com>
2022-11-21 10:31:44 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,12 @@
+class factorial{
+	
+	public static void main(String args[]){
+		
+		int n=5,fact=1;
+		
+		for(int i=1;i<=n;i++)
+			fact=fact*i;
+		
+		System.out.println("Factorial of "+n+"is = "+fact);
+	}
+}
